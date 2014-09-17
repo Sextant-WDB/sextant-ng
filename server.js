38530f22f3ce8ff21b1257579b5cb390d3bb41c6
David Davidson <daviddavidson.1988@gmail.com>
2014-09-16 21:07:37 -0700

    Tie in our fancy new authorization

--- server.js
+++ server.js
@@ -1,21 +1,35 @@
 'use strict';
+
 var http = require('http');
 var express = require('express');
 var bodyparser = require('body-parser');
 var mongoose = require('mongoose');
+var passport = require('passport');
 
-mongoose.connect(process.env.MONGOLAB_URI || process.env.MONGO_URL || 'mongodb://localhost/sextant');
-
+// Config
 var app = express();
 app.use(express.static(__dirname + '/build'));
 app.use(bodyparser.json());
 
-require('./routes/express-routes')(app);
+mongoose.connect(process.env.MONGOLAB_URI || process.env.MONGO_URL || 'mongodb://localhost/sextant');
 
+// Auth
+app.set('jwtTokenSecret', process.env.JWT_SECRET || 'changeMeChangeMeChangeMe');
+app.set('secret', process.env.SECRET || 'changeMeChangeMeChangeMe'); // We never actually use this; passport just requires it
+
+app.use(passport.initialize());
+require('./lib/auth/passport')(passport);
+
+// Routing
+var jwtAuth = require('./lib/auth/jwt-auth')(app);
+
+require('./routes/user-routes')(app, passport);
+require('./routes/express-routes')(app, jwtAuth.auth);
+
+// Init
 var server = http.createServer(app);
-
 app.set('port', process.env.PORT || 3000);
 
 server.listen(app.get('port'),function(){
-  console.log('Server has started on ' + app.get('port'));
+  console.log('Server has started on port %d', app.get('port'));
 });
