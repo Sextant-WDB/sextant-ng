bf96d2d642a9f146f6db40024a8d00df72ef8b5c
David Davidson <daviddavidson.1988@gmail.com>
2014-09-17 15:01:51 -0700

    Tighten up the HTTP helper

--- app/services/http-service.js
+++ app/services/http-service.js
@@ -10,16 +10,19 @@ module.exports = function(app) {
 
 			var promise = $http[method]('/api/0_0_1/data/' + params.id, params.data)
 			.error(function(error, status) {
+
 				console.log('Error in http ' + method + ': ' + error + ' | status ' + status);
 				if (status === 401) {
 					console.log('401 error in httpService!');
 					$location.path('/signin');
 				}
+
 			});
 
 			return promise;
 		},
 
+		// Specific verbs
 		httpVerbs = {
 
 			get: function() {
@@ -27,30 +30,15 @@ module.exports = function(app) {
 			},
 
 			post: function(data) {
-				return http('post', {
-					data: {
-						url: data.url,
-						pageViews: data.pageViews,
-						time: data.time
-					}
-				});
+				return http('post', data);
 			},
 
 			put: function(data) {
-				return http('put', {
-					data: {
-						url: data.url,
-						pageViews: data.pageViews,
-						time: data.time
-					},
-					id: data._id
-				});
+				return http('put', data);
 			},
 
 			delete: function(data) {
-				return http('delete', {
-					id: data._id
-				});
+				return http('delete', data);
 			}
 
 		};
