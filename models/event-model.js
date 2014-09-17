4aa1284543cec8fce29e32d13735cb65a781e2e0
David Davidson <daviddavidson.1988@gmail.com>
2014-09-16 21:06:59 -0700

    Delete old files

--- models/event-model.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var mongoose = require('mongoose');
-var eventScheme = mongoose.Schema({
-		url: String,
-		eventType: String
-	});
-
-module.exports = mongoose.model('EventModel', eventScheme);
