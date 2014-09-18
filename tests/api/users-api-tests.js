5f0344db5d51424d5cc03d59a3c4174d474db10a
David Davidson <daviddavidson.1988@gmail.com>
2014-09-18 11:08:17 -0700

    Add basic tests for the users REST route

---
+++ tests/api/users-api-tests.js
@@ -0,0 +1,104 @@
+var port = require('../../server').port;
+
+var chai = require('chai');
+var chaihttp = require('chai-http');
+chai.use(chaihttp);
+var expect = chai.expect;
+
+var firstID, secondID;
+
+describe('The users REST API', function() {
+
+	it('Can create a new user', function(done) {
+
+		chai.request('http://localhost:' + port)
+		.post('/api/0_0_1/users')
+		.req(function(req) {
+			req.send({
+				"email": "fakeaddress@fake.com",
+				"password": "123"
+			});
+		})
+		.res(function(res) {
+			expect(res).to.have.status(200);
+			firstID = res.body._id;
+			done();
+		});
+
+	});
+
+	it('Returns a JWT', function(done) {
+
+		chai.request('http://localhost:' + port)
+		.post('/api/0_0_1/users')
+		.req(function(req) {
+			req.send({
+				"email": "fakeaddress2@fake.com",
+				"password": "123"
+			});
+		})
+		.res(function(res) {
+			expect(res).to.have.status(200);
+			expect(res.body).to.have.property('jwt');
+			secondID = res.body._id;
+			done();
+		});
+
+	});
+
+	it('Can update a user', function(done) {
+		chai.request('http://localhost:' + port)
+		.put('/api/0_0_1/users/' + secondID)
+		.req(function(req) {
+			req.send({
+				basic: {
+					"email": "fakeaddress3@fake.com"
+				}
+			});
+		})
+		.res(function(res) {
+			expect(res).to.have.status(202);
+			expect(res.body.basic.email).to.eql('fakeaddress3@fake.com');
+			done();
+		});
+
+	});
+
+	it('Rejects duplicate users', function(done) {
+
+		chai.request('http://localhost:' + port)
+		.post('/api/0_0_1/users')
+		.req(function(req) {
+			req.send({
+				"email": "fakeaddress@fake.com",
+				"password": "123"
+			});
+		})
+		.res(function(res) {
+			expect(res).to.have.status(401);
+			expect(res.body.message).to.eql('cannot create user');
+			done();
+		});
+
+	});
+
+	it('Can delete users', function(done) {
+
+		chai.request('http://localhost:' + port)
+		.del('/api/0_0_1/users/' + firstID)
+		.res(function(res) {
+			expect(res).to.have.status(200);
+			expect(res.body.message).to.eql('deleted');
+		});
+
+		chai.request('http://localhost:' + port)
+		.del('/api/0_0_1/users/' + secondID)
+		.res(function(res) {
+			expect(res).to.have.status(200);
+			expect(res.body.message).to.eql('deleted');
+			done();
+		});
+
+	});
+
+});
