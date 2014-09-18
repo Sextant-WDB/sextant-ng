8438faacd13ee1263150403d18f1b2d4c9736722
David Davidson <daviddavidson.1988@gmail.com>
2014-09-17 20:34:23 -0700

    Get 'filesToWatch' up to date

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,7 +11,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-browserify');
 
   var allJavaScriptFilePaths = [
-    'app/js/**/*.js',
+    'app/**/*.js',
     'models/**/*.js',
     'routes/**/*.js',
     'lib/**/*.js',
