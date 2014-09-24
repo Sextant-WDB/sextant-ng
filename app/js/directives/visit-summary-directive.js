376536bf8f31701d19ad8b15f99ccb2b56785b7d
Walker Malling <walker.malling@live.com>
2014-09-24 12:12:06 -0700

    add bounce rate calculation to visits summary directive

--- app/js/directives/visit-summary-directive.js
+++ app/js/directives/visit-summary-directive.js
@@ -4,7 +4,18 @@ module.exports = function(app) {
   app.directive('visitSummary', function() {
     return {
       restrict: 'E',
-      templateUrl: 'templates/visit-summary-template.html'
+      templateUrl: 'templates/visit-summary-template.html',
+      controller: function($scope){
+        $scope.$watch('visits', function(){
+          if( $scope.visits ){
+            var bounces = 0;
+            $scope.visits.forEach(function(el){
+              if( el.events.length === 1 ) bounces ++;
+            });
+            $scope.bounceRate = (bounces / $scope.visits.length * 100).toFixed(1);
+          }
+        });
+      }
     };
   });
 };
