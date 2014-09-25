8125df5cc2b51ee947f69bab5e17bc7a077169ce
David Davidson <daviddavidson.1988@gmail.com>
2014-09-24 21:29:20 -0700

    Add missing space

--- app/js/controllers/data-controller.js
+++ app/js/controllers/data-controller.js
@@ -27,7 +27,7 @@ module.exports = function(app) {
         $scope.selectedDomain = domain_id;
 
         visitService.get(domain_id.toString())
-          .success(function(visits){
+          .success(function(visits) {
             $scope.visits = visits;
           });
       };
