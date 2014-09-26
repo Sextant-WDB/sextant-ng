2e86e8897cc5aadeac2ece709b7a2e242bec84ee
Brock Beaudry <brock.beaudry@gmail.com>
2014-09-26 00:19:22 -0700

    Update graph with heat map

--- build/scripts.js
+++ build/scripts.js
@@ -25145,9 +25145,11 @@ module.exports = function(app) {
       var force = $scope.d3.layout.force()
         .nodes($scope.d3.values(data.nodes))
         .links(data.links)
+        .alpha(0.5)
+        .linkDistance([120])
+        .linkStrength(1)
+        .charge([-1200])
         .size([width, height])
-        .linkDistance(60)
-        .charge(-300)
         .on('tick', tick)
         .start();
 
@@ -25155,11 +25157,25 @@ module.exports = function(app) {
         .attr('width', width)
         .attr('height', height);
 
+      // svg.append('svg:defs').selectAll('marker')
+      //     .data(['end'])
+      //   .enter().append('svg:marker')
+      //     .attr('id', String)
+      //     .attr('viewBox', '0 -5 10 10')
+      //     .attr('refX', 15)
+      //     .attr('refY', -1.5)
+      //     .attr('markerWidth', 6)
+      //     .attr('markerHeight', 6)
+      //     .attr('orient', 'auto')
+      //   .append('svg:path')
+      //     .attr('d', 'M0,-5L10,0L0,5');
+
       var link = svg.selectAll('.link')
           .data(force.links())
-        .enter().append('line')
+        .enter().append('svg:path')
           .attr('class', 'link')
           .style('stroke-width', function(d) { return (d.count / $scope.maxWeight) * 5; });
+
 
       var node = svg.selectAll('.node')
           .data(force.nodes())
@@ -25170,21 +25186,32 @@ module.exports = function(app) {
       node.append('circle')
         .attr('r', function(d) {
           if(!!d.count) return 10 + (d.count / $scope.landingMax * 10);
-
           return 10;
+        })
+        .style('fill', function(d){
+          var w = (!!d.count) ?  255 - (255 * d.count / $scope.landingMax) : 255;
+          return 'rgb(215,'+ w+','+ w +')';
         });
 
       node.append('text')
-        .attr('x', 12)
-        .attr('dy', '.35em')
+        .attr('x', 20)
+        .attr('y', -10)
         .text(function(d) { return d.name; });
 
+
+
       function tick() {
-        link
-            .attr('x1', function(d) { return d.source.x; })
-            .attr('y1', function(d) { return d.source.y; })
-            .attr('x2', function(d) { return d.target.x; })
-            .attr('y2', function(d) { return d.target.y; });
+        link.attr('d', function(d) {
+          var dx = d.target.x - d.source.x,
+              dy = d.target.y - d.source.y,
+              dr = Math.sqrt(dx * dx + dy * dy);
+          return 'M' +
+            d.source.x + ',' +
+            d.source.y + 'A' +
+            dr + ',' + dr + ' 0 0,1 ' +
+            d.target.x + ',' +
+            d.target.y;
+        });
 
         node
             .attr('transform', function(d) { return 'translate(' + d.x + ',' + d.y + ')'; });
