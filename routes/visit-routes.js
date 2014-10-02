32abeb8e859bf583ca855f9f7d8f504763248563
David Davidson <daviddavidson.1988@gmail.com>
2014-10-02 08:56:02 -0700

    Add an endpoint for clearing visit data

--- routes/visit-routes.js
+++ routes/visit-routes.js
@@ -3,12 +3,15 @@
 var Visit = require('../models/visit-model');
 var Domain = require('../models/domain-model');
 
-/**
- * Read all visits to a given domain (authenticated)
- */
-
 module.exports = function(app, jwtAuth) {
-	app.get('/api/0_0_1/visits/:domainId', jwtAuth, function(req, res) {
+
+	var api = '/api/0_0_1/visits';
+
+	/**
+	 * Read all visits to a given domain (authenticated)
+	 */
+
+	app.get(api + '/:domainId', jwtAuth, function(req, res) {
 	  Domain.findOne({
 	    _id: req.params.domainId.toString(),
 	    authorizedUsers: { $all: [ req.user._id ] }
@@ -20,4 +23,22 @@ module.exports = function(app, jwtAuth) {
 	    });
 	  });
 	});
+
+
+  /**
+   * Delete all visits corresponding to a given domain (authenticated)
+   */
+
+  app.delete(api + '/:domainName(*)', jwtAuth, function(req, res) {
+    console.log('delete fired with domain ' + req.params.domainName);
+    Visit.remove({ host: req.params.domainName.toString() }, function(err) {
+      if (err) {
+        return res.status(500).json(err);
+      }
+      return res.status(200).json({
+        'message': 'deleted'
+      });
+    });
+  });
+
 };
