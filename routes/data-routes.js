bedad1d4b87b4f6a7d61d8e41b0784d9ddeb6f53
David Davidson <daviddavidson.1988@gmail.com>
2014-10-02 11:11:47 -0700

    Remove some unnecessary endpoints

--- routes/data-routes.js
+++ routes/data-routes.js
@@ -28,69 +28,20 @@ module.exports = function(app, cors) {
 
     if (req.body.host && req.body.host.charAt(req.body.host.length - 1) === '/') {
       req.body.host = req.body.host.slice(0, -1); // trim the last char
-      // console.log('new req.body.host: ' + req.body.host);
     }
 
     Visit.update({ 'session_id' : req.body.sessionID }, { $pushAll: { events: req.body.events }}, function(err, records) {
 
       if(!records) {
-        // console.log('No records, creating new event');
 
         var newEvent = new Visit(req.body);
         newEvent.save(function(err, dbResponse) {
           if (err) return res.status(500).json(err);
-          // console.log('new visit saved');
           return res.status(200).json(dbResponse);
         });
       } else {
-        // console.log('Found visit and updated');
         return res.status(200).end();
       }
-    });
-  });
-
-  /**
-   * Update single piece of data
-   */
-
-  app.put(api + '/:id', function(req, res) {
-    var data = req.body;
-    delete data._id;
-    Visit.findOneAndUpdate({ '_id': req.params.id }, data, function(err, dbResponse) {
-      if (err) {
-        return res.status(500).json(err);
-      }
-      return res.status(202).json(dbResponse);
-    });
-  });
-
-  /**
-   * Delete single piece of data
-   */
-
-  app.delete(api + '/delete/:id', function(req, res) {
-    Visit.remove({ '_id': req.params.id }, function(err) {
-      if (err) {
-        return res.status(500).json(err);
-      }
-      return res.status(200).json({
-        'message': 'deleted'
-      });
-    });
-  });
-
-  /**
-   * For development only: delete all data!!1111
-   */
-
-  app.delete(api + '/deleteAll', function(req, res) {
-    Visit.remove({}, function(err) {
-      if (err) {
-        return res.status(500).json(err);
-      }
-      return res.status(200).json({
-        'message': 'deleted'
-      });
     });
   });
 
